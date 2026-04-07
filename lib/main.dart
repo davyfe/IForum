c0452ce15e2ee42094697446328e756fc4a3cf3f
Davy <dfs26@aluno.ifal.edu.br>
2026-04-07 10:07:28 -0300

    Mudei o nome para 'IForum-teste'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'IForum01',
+      title: 'IForum-teste',
       theme: ThemeData(
         // This is the theme of your application.
         //
