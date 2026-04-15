5d28d949e6a3ab293b893a885ece2e00f33125d5
Davy <dfs26@aluno.ifal.edu.br>
2026-04-15 00:07:50 -0300

    teste 1: apenas o appbar com a cor

--- lib/home.dart
+++ lib/home.dart
@@ -13,14 +13,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-          title: TextField(
-            decoration: InputDecoration(
-              hintText: 'Pesquisar',
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
-            ),
-          ), backgroundColor: Color(0xFF2E7D32)
-      ), // appbar
+      appBar: AppBar(backgroundColor: Color(0xFF2E7D32)), // appbar
       body: ListView(
         children: [
           buildHeader(), // appbar modificado
