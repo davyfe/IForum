0d1c93b2f32ee4245a372bae7aaf95143bd5e267
Davy <dfs26@aluno.ifal.edu.br>
2026-04-21 12:54:42 -0300

    Aqui altera a borda arredondada dos cantos

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -28,7 +28,7 @@ class _HomeState extends State<Home> {
                 filled: true,
                 contentPadding: EdgeInsets.only(right: 16),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25),
+                  borderRadius: BorderRadius.circular(20),
                   borderSide: BorderSide.none,
                 ),
               ),
